127e26e387cdd4f902c2b16bf482c8dae893fb5c
Aviv <acohn@dgitech.com>
2023-04-03 16:09:57 -0400

    init

---
+++ LutronOutputControl/LutronOutput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+
+namespace LutronOutputControl
+{
+    public class LutronOutput
+    {
+        static Dictionary<string, int> loadDictionary = new Dictionary<string, int>();
+        
+        public static void GetLoadDictionary(string path)
+        {
+            string jsonString = System.IO.File.ReadAllText(path);
+            loadDictionary = JsonSerializer.Deserialize<Dictionary<string, int>>(jsonString);
+        }
+        
+        public static string Close(string loadName)
+        {
+            return $"#OUTPUT,{loadDictionary[loadName]},3";
+        }
+        
+        public static string Open(string loadName)
+        {
+            return $"#OUTPUT,{loadDictionary[loadName]},2";
+        }
+        
+
+    }
+}
